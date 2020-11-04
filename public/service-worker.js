99a2fc930e9b72241d98e2c36e92f7b8d0a6911a
Maged Abdelsalam <dagadego@gmail.com>
2020-11-03 22:21:08 -0800

    Added PWA

---
+++ public/service-worker.js
@@ -0,0 +1,93 @@
+const FILES_TO_CACHE = [
+
+  ];
+  
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const iconSizes = ["72", "96", "128", "144", "152", "192", "384", "512"];
+const iconFiles = iconSizes.map(
+  (size) => `/assets/img/icons/icon-${size}.png`
+);
+
+const staticFilesToPreCache = [
+    "/",
+    "/manifest.json",
+    "/favicon.ico",
+    "/assets/css/style.css",
+    "/assets/js/index.js",
+    "/assets/img/daniel.png",
+    "/assets/img/hero.png",
+    "/assets/img/jae.png",
+    "/assets/img/janelle.png",
+    "/assets/img/maged.png",
+    "https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap",
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css",
+    "https://code.jquery.com/jquery-3.2.1.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js",
+].concat(iconFiles);
+
+
+// install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(staticFilesToPreCache);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+  const {url} = evt.request;
+  if (url.includes("/api/pods") || url.includes("/api/kids")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+  } else {
+    // respond from static cache, request is not for /api/*
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+  }
+});
